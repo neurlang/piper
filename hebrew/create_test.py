c753d46ab3adae6e2e015ff5f1425268e4de926b
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-05-05 05:04:13 +0300

    Update Hebrew phoneme entry: correct formatting of "letel" for consistency

--- hebrew/create_test.py
+++ hebrew/create_test.py
@@ -19,7 +19,7 @@ EspeakWrapper.set_data_path(espeakng_loader.get_data_path())
 phonemes = [
     "ʃalˈom olˈam! mˈa koʁˈe?",
     "bˈo teʁˈed, toχˈal ktsˈat tˈeʁed. ʔˈejze tχinˈa! jihjˈe tχˈina tovˈa! bˈo niʃtˈe bˈiʁa beʔˈiʁ habiʁˈa! hˈu pitˈa ʔotˈi leʔeχˈol pˈita ʃawˈaʁma!",
-    "sˈimu lˈev, nosʔˈim jekaʁˈim. haʁakˈevet letˈel ʔavˈiv meʁkˈaz tikanˈes leʁatsˈif mispˈaʁ ʃalˈoʃ beʔˈod mispˈaʁ dakˈot. ʔˈana hitʁaχakˈu miktsˈe haʁatsˈif vehamtˈinu meʔaχoʁˈej hakˈav hatsahˈov.",
+    "sˈimu lˈev nosʔˈim jekaʁˈim. haʁakˈevet letel ʔavˈiv meʁkˈaz tikanˈes leʁatsˈif mispˈaʁ ʃalˈoʃ beʔˈod mispˈaʁ dakˈot. ʔˈana hitʁaχakˈu miktsˈe haʁatsˈif vehamtˈinu meʔaχoʁˈej hakˈav hatsahˈov.",
     phonemizer.phonemize('Just kicking back with some good music, letting the vibes do their thing.')
 ]
 
